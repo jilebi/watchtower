d8bee1bce4419319d1a9b552a77ade0062a324f6
Thiyag Krishna <thiyagaraj@gmail.com>
2016-12-12 00:02:55 -0500

    Chrome notification test

--- src/core/background.js
+++ src/core/background.js
@@ -1,1 +1,8 @@
-var test;
+
+var opt = {
+  type: "basic",
+  title: "Watchtower",
+  message: "This is a test message",
+  iconUrl: "../icons/watchtower-48.png"
+}
+chrome.notifications.create(null, opt, null);
