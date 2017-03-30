44f3d5b1935fef4856f3f4a811c7afe7a1d2318b
Thiyagaraj Krishna <thiyagaraj@gmail.com>
2017-03-30 19:09:38 -0400

    Interim, permission error - possible chrome bug?

--- src/core/background.js
+++ src/core/background.js
@@ -1,3 +1,77 @@
+var Watchtower = function () {
+  var tabSettings = settings.tabSettings;
+  this.openTabs = [];
+  this.activeTabIndex = 0;
+  this.init = function () {
+    this.closeAllTabs();
+    this.initiateTabs();
+    //this.displayManager();
+  };
+  this.addToOpenTabs = function(tabInfo) {
+    this.openTabs.push(tabInfo);
+  }
+  this.closeAllTabs = function () {
+    /* Close all existing tabs */
+    var queryInfo = {};
+    queryInfo.url = settings.tabSelector;
+
+    chrome.tabs.query(queryInfo, function (tabs) {
+      for (var i = 0; i < tabs.length; i++) {
+        chrome.tabs.remove(tabs[i].id);
+      }
+    });
+    /* End close all existing tabs */
+  };
+  this.initiateTabs = function () {
+
+    for (var i = 0; i < tabSettings.length; i++) {
+      var createProps = {};
+      createProps.url = tabSettings[i].url;
+      console.log("dumping tabSettings[" + i + "]", tabSettings[i]);
+      //mark only the first tab as active
+      createProps.active = (i == 0) ? true : false;
+      console.log("dumping createProps in iteration " + i + " :", createProps);
+      (function (i, that) {
+        console.log("before: openTabs = ", that.openTabs);
+        chrome.tabs.create(createProps, function (tab) {
+          console.log("Tab with id :" + tab.id + " created for i " + i);
+          console.log("createProps = ", createProps);
+          var tabInfo = {};
+          tabInfo.tabId = tab.id;
+          tabInfo.tabIndex = i;
+          tabInfo.url = tabSettings[i].url;
+          tabInfo.duration = tabSettings[i].duration;
+          tabInfo.kaput = false;
+          console.log("openTabs = ", that.openTabs);
+          that.openTabs.push(tabInfo);
+          if (i == (tabSettings.length - 1)) {
+            that.displayManager();
+          }
+        });
+      })(i, this);
+    }
+  };
+
+  this.displayManager = function () {
+    console.log("in displayManager()");
+    console.log("In displayManager()", this.openTabs[this.activeTabIndex]);
+    var jumpToTab = this.openTabs[this.activeTabIndex];
+    var tabId = jumpToTab.tabId;
+    var jumpToTabProps = {
+      "active": true
+    };
+    chrome.tabs.update(tabId, jumpToTabProps, function () {
+      //set the stage for the next tab
+      if (this.activeTabIndex == (tabSettings.length - 1)) {
+        this.activeTabIndex = 0;
+      } else {
+        this.activeTabIndex++;
+      }
+      window.setTimeout(this.displayManager, (jumpToTab.duration * 1000));
+    });
+  }
+}
+
 
 var opt = {
   type: "basic",
@@ -10,9 +84,9 @@ chrome.notifications.create(null, opt, null);
 var settings = {
   "tabSettings": [
     {
-      "url": "http://sketchduty.com",
+      "url": "http://apache.org",
       "duration": "5",
-      "refresh" : true
+      "refresh": true
     },
     {
       "url": "https://github.com",
@@ -26,64 +100,5 @@ var settings = {
   "tabSelector": ["http://sketchduty.com/*", "https://github.com/*"]
 }
 
-/* Close all existing tabs */
-var queryInfo = {};
-queryInfo.url = settings.tabSelector;
 
-chrome.tabs.query(queryInfo, function (tabs) {
-  for (var i = 0; i < tabs.length; i++) {
-    chrome.tabs.remove(tabs[i].id);
-  }
-});
-/* End close all existing tabs */
-
-
-var tabSettings = settings.tabSettings;
-var openTabs = [];
-var activeTabIndex = 0;
-
-for (var i = 0; i < tabSettings.length; i++) {
-  var createProps = {};
-  createProps.url = tabSettings[i].url;
-  console.log("dumping tabSettings[" + i + "]", tabSettings[i]);
-  //mark only the first tab as active
-  createProps.active = (i == 0) ? true : false;
-  console.log("dumping createProps in iteration " + i + " :", createProps);
-  (function (i) {
-    chrome.tabs.create(createProps, function (tab) {
-      console.log("Tab with id :" + tab.id + " created for i " + i);
-      console.log("createProps = ", createProps);
-      var tabInfo = {};
-      tabInfo.tabId = tab.id;
-      tabInfo.tabIndex = i;
-      tabInfo.url = tabSettings[i].url;
-      tabInfo.duration = tabSettings[i].duration;
-      tabInfo.kaput = false;
-      openTabs.push(tabInfo);
-      if (i == (tabSettings.length - 1)) {
-        displayManager();
-      }
-    });
-  })(i);
-}
-
-
-var displayManager = function () {
-  console.log("In displayManager()", openTabs[activeTabIndex]);
-  var jumpToTab = openTabs[activeTabIndex];
-  var tabId = jumpToTab.tabId;
-  var jumpToTabProps = {
-    "active": true
-  };
-  chrome.tabs.update(tabId, jumpToTabProps, function () {
-    //set the stage for the next tab
-    if (activeTabIndex == (tabSettings.length - 1)) {
-      activeTabIndex = 0;
-    } else {
-      activeTabIndex++;
-    }
-
-    
-    window.setTimeout(displayManager, (jumpToTab.duration * 1000));
-  });
-};
+new Watchtower().init();
